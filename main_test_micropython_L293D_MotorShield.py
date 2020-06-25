a0928e2934c008db192667adf2f8d5e09a0ee965
dubuku <61646407+Dubuku00@users.noreply.github.com>
2020-06-25 17:03:14 +0530

    Add files via upload
    Test main file.
    Rename it to main and test with L293D driver file

---
+++ main_test_micropython_L293D_MotorShield.py
@@ -0,0 +1,36 @@
+"""
+To test DC motors in Motorshield channel 1 and Steper motor in channel 2
+
+"""
+
+import pyb
+from pyb import Pin, delay
+from PYB_L239D import DriveDC
+from PYB_L239D import DriveStepper
+
+#DC motor initialize
+LMA = DriveDC("LMA", 'X1')
+LMB = DriveDC("RMB", 'X2')
+
+#Stepper initialize
+STP2 = DriveStepper("STP2", 100, 'X3', 'X4')
+
+#SPI initialize
+STP2.setSPI('Y', 'Y1')  # SPI init - one time
+print('sr called')	
+
+LMA.setSpeed('LMA', 100)	
+print('speed called')
+LMA.run('FORWARD')
+#RMB.run('FORWARD')
+delay(2000)
+LMA.run('RELEASE')
+
+STP2.StepperSpeed("STP2", 200)
+STP2.StepperStep( "W", 50, "FORWARD", "DOUBLE")	
+print('back to main')	
+delay(2000)
+STP2.StepperSpeed("STP2", 240)
+STP2.StepperStep( "W", 100, "BACKWARD", "COMBO")	
+STP2.StepperStep( "R", 100, "RELEASE", "COMBO")	
+print('back to main again')	
